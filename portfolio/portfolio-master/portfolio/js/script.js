6f4f72c276a82c18283f7d497c9fb84a30f4330e
ratshin38 <52753337+ratshin38@users.noreply.github.com>
2019-10-27 17:08:55 +0100

    Add one page navigation

---
+++ portfolio/portfolio-master/portfolio/js/script.js
@@ -0,0 +1,37 @@
+/*document.querySelector("").addEventListener("click",function()
+
+==> cacher le blur 
+==> cacher le welcolme content 
+==> afficher le nav
+==> afficher la page du slide)
+*/
+
+/* event pour afficher skill page en cliquant sur la slider skill*/
+document.querySelectorAll(".skill").forEach(element =>{element.addEventListener("click", function(){
+    document.querySelector(".content_bckgnd").classList.add("none");
+    document.querySelector(".contact_page").classList.add("none");
+    document.querySelector(".education_page").classList.add("none");
+    document.querySelector(".test_nav").classList.remove("none");
+    document.querySelector(".skill_page").classList.remove("none");
+})});
+document.querySelectorAll(".education").forEach(element =>{element.addEventListener("click", function(){
+    document.querySelector(".content_bckgnd").classList.add("none");
+    document.querySelector(".contact_page").classList.add("none");
+    document.querySelector(".skill_page").classList.add("none");
+    document.querySelector(".test_nav").classList.remove("none");
+    document.querySelector(".education_page").classList.remove("none");
+})});
+document.querySelectorAll(".contact").forEach(element =>{element.addEventListener("click", function(){
+    document.querySelector(".content_bckgnd").classList.add("none");
+    document.querySelector(".skill_page").classList.add("none");
+    document.querySelector(".education_page").classList.add("none");
+    document.querySelector(".test_nav").classList.remove("none");
+    document.querySelector(".contact_page").classList.remove("none");
+})});
+document.querySelectorAll(".welcome").forEach(element =>{element.addEventListener("click", function(){
+    document.querySelector(".contact_page").classList.add("none");
+    document.querySelector(".skill_page").classList.add("none");
+    document.querySelector(".education_page").classList.add("none");
+    document.querySelector(".test_nav").classList.add("none");
+    document.querySelector(".content_bckgnd").classList.remove("none");
+})});
