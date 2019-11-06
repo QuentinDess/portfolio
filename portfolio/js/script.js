5267f936035fefa58941eb1c1f886e8eb6261670
ratshin38 <52753337+ratshin38@users.noreply.github.com>
2019-11-06 22:21:00 +0100

    button education page event

--- portfolio/js/script.js
+++ portfolio/js/script.js
@@ -31,3 +31,29 @@ document.querySelectorAll(".welcome").forEach(element =>{element.addEventListene
     document.querySelector(".education_page").classList.add("none");
     document.querySelector(".content_bckgnd").classList.remove("none");
 })});
+// event pour action cliquer sur le boutton affiche le paragraphe associer de la page education 
+
+document.querySelector(".it_button").addEventListener("click", () =>{
+    document.querySelector(".it_education").classList.toggle("none");
+    document.querySelector(".it_button").classList.toggle("margin_after");
+    document.querySelector(".it_button").classList.toggle("margin_before");
+
+});
+document.querySelector(".op_button").addEventListener("click", () =>{
+    document.querySelector(".op_education").classList.toggle("none");
+    document.querySelector(".op_button").classList.toggle("margin_after");
+    document.querySelector(".op_button").classList.toggle("margin_before");
+
+});
+document.querySelector(".isema_button").addEventListener("click", () =>{
+    document.querySelector(".isema_education").classList.toggle("none");
+    document.querySelector(".isema_button").classList.toggle("margin_after");
+    document.querySelector(".isema_button").classList.toggle("margin_before");
+
+});
+document.querySelector(".bts_button").addEventListener("click", () =>{
+    document.querySelector(".bts_education").classList.toggle("none");
+    document.querySelector(".bts_button").classList.toggle("margin_after");
+    document.querySelector(".bts_button").classList.toggle("margin_before");
+
+});
